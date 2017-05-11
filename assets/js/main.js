03bf2f7394c7a80ad456ef20d146037243ad4036
Melissa <melissa.pacheco.martinez@gmail.com>
2017-05-11 12:18:33 -0300

    avanzando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-main.js
+document.getElementById("toggle").addEventListener("click", function(event){
+	event.preventDefault();
+	document.getElementById("nav-header").classList.toggle("open");
+	document.getElementById("body").classList.toggle("overflow-hidden");	
+});
+
+var currentScroll = window.pageYOffSet || document.body.scrollTop;
